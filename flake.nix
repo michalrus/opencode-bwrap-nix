951cb237280be42aecd67f9f4bcf7b5126e75f17
Michal Rus <m@michalrus.com>
2026-02-03 14:19:52 +0100

    chore: remove `opencode-vm`

--- flake.nix
+++ flake.nix
@@ -2,8 +2,6 @@
   inputs = {
     nixpkgs-2511.url = "github:NixOS/nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager-2511.url = "github:nix-community/home-manager/release-25.11";
-    home-manager-2511.inputs.nixpkgs.follows = "nixpkgs-2511";
   };
 
   outputs = inputs: {
@@ -14,11 +12,6 @@
       ] (system: let
         inherit (inputs.nixpkgs-2511.legacyPackages.${system}) callPackage;
       in {
-        opencode-vm = callPackage ./opencode-vm {
-          nixpkgs = inputs.nixpkgs-2511;
-          inherit (inputs) nixpkgs-unstable;
-          home-manager = inputs.home-manager-2511;
-        };
         opencode-bwrap = callPackage ./opencode-bwrap {
           inherit (inputs) nixpkgs-unstable;
         };
