39ada60f4c4a536f12795c24ee48b3a583c8ed18
Michal Rus <m@michalrus.com>
2026-02-13 02:45:40 +0100

    feat: define the `opencode.json` config in Nix, too

--- opencode-bwrap/default.nix
+++ opencode-bwrap/default.nix
@@ -5,6 +5,23 @@
 }: let
   unsafe = nixpkgs-unstable.legacyPackages.${pkgs.stdenv.hostPlatform.system}.opencode;
 
+  config = {
+    "$schema" = "https://opencode.ai/config.json";
+    share = "disabled";
+    theme = "solarized";
+    lsp = false;
+    tui = {
+      diff_style = "stacked";
+    };
+    autoupdate = false;
+    experimental = {
+      disable_paste_summary = true;
+    };
+    instructions = [
+      ./preamble.md
+    ];
+  };
+
   bashrc = pkgs.writeText "opecode-bashrc" ''
     # Commands that should be applied only for interactive shells.
     [[ $- == *i* ]] || return
@@ -152,6 +169,7 @@
         --setenv LOCALE_ARCHIVE "$LOCALE_ARCHIVE"
         --setenv LOCALE_ARCHIVE_2_27 "$LOCALE_ARCHIVE_2_27"
         --setenv OPENCODE_DISABLE_LSP_DOWNLOAD "true"
+        --setenv OPENCODE_CONFIG ${pkgs.writeText "config.json" (builtins.toJSON config)}
       )
 
       for d in "''${persist_dirs[@]}" ; do
@@ -163,11 +181,6 @@
         touch "$sandbox_home"/"$f"
         bwrap_opts+=( --bind "$sandbox_home"/"$f" "$HOME"/"$f" )
       done
-
-      bwrap_opts+=(
-        --ro-bind "${./AGENTS.md}" "$HOME"/.config/opencode/AGENTS.md
-        --ro-bind "${./opencode.json}" "$HOME"/.config/opencode/opencode.json
-      )
 
       rw_opts=()
       ro_git_opts=()
