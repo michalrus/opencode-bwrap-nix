64a0148e18ba86faea466810e3719acafb2979cf
Michal Rus <m@michalrus.com>
2026-03-22 13:04:59 +0000

    feat: add `opencode-vm`

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs-2511.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager-2511.url = "github:nix-community/home-manager/release-25.11";
+    home-manager-2511.inputs.nixpkgs.follows = "nixpkgs-2511";
+  };
+
+  outputs = inputs: {
+    packages =
+      inputs.nixpkgs-2511.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+      ] (system: let
+        inherit (inputs.nixpkgs-2511.legacyPackages.${system}) callPackage;
+      in {
+        opencode-vm = callPackage ./opencode-vm {
+          nixpkgs = inputs.nixpkgs-2511;
+          inherit (inputs) nixpkgs-unstable;
+          home-manager = inputs.home-manager-2511;
+        };
+      });
+  };
+}
